4fadf578f231655f20b705f3e5a15c5d497609ce
Your NameStepan <lukachina.stepan@mail.com>
2019-11-12 23:14:21 +0200

    fixed mobile vers + add icons for mob

--- js/mainuser.js
+++ js/mainuser.js
@@ -7,6 +7,9 @@
  $("#Email").html ('<b>Email: </b>' + user.email);
  $("#Phone").html ('<b>Mob: </b>' + user.telephon);
  
+ $("#Emailb").html ('<b>Email: </b>' + user.email);
+ $("#Phoneb").html ('<b>Mob: </b>' + user.telephon);
+ 
  $(".morep").html ('<b>Адреса: </b>' + user.adress);
  $(".otherp").html ('<b>Інша інформація про користувача: </b>' + user.biog);
  
@@ -14,21 +17,41 @@
   $("#other").addClass ('active');
     $("#more").removeClass ('active');
 	  $("#records").removeClass ('active');
+	  $("#contactbot").removeClass ('active');
   
   $(".morep").css('display', 'none');
   $(".recordsp").css ('display', 'none');
+  $(".contactbotp").css ('display', 'none');
  $(".otherp").css ('display', 'block');
+ 
  $(".navmenutittle").text('Інші дані')
  })
+ 
+ $("#contactbot").click ( function () {
+  $("#contactbot").addClass ('active');
+    $("#more").removeClass ('active');
+	  $("#records").removeClass ('active');
+	  $("#other").removeClass ('active');
+  
+  $(".morep").css('display', 'none');
+  $(".recordsp").css ('display', 'none');
+ $(".otherp").css ('display', 'none');
+  $(".contactbotp").css ('display', 'block');
+ $(".navmenutittle").text('Контактні дані')
+ })
+ 
+ 
+ 
  
   $("#more").click ( function () {
 	    $("#other").removeClass ('active');
     $("#more").addClass('active');
 	  $("#records").removeClass ('active');
-	  
+	   $("#contactbot").removeClass ('active');
 
   $(".morep").css ('display', 'block');
   $(".recordsp").css ('display', 'none');
+  $(".contactbotp").css ('display', 'none');
  $(".otherp").css ('display', 'none');
  $(".navmenutittle").text('Додаткова інформація')
  })
@@ -38,11 +61,12 @@
 	   $("#other").removeClass ('active');
     $("#more").removeClass('active');
 	    $("#records").addClass('active');
-	  
+	   $("#contactbot").removeClass ('active');
 	  
 
   $(".morep").css ('display', 'none');
   $(".recordsp").css ('display', 'block');
+  $(".contactbotp").css ('display', 'none');
  $(".otherp").css ('display', 'none');
  $(".navmenutittle").text('Записи')
  })
